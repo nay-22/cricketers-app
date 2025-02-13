d4f504aaae83f018b896d8df71f0a924f8d1634f
Nayan <nayan92001@gmail.com>
2025-02-13 14:28:35 +0530

    feat(CricketDetails): extract player details using params and context and render it via CricketerCard

--- src/pages/CricketerDetails.tsx
+++ src/pages/CricketerDetails.tsx
@@ -1,5 +1,15 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import AppContext from "../context/AppContext";
+import CricketerCard from "../components/cards/CricketerCard";
+
 const CricketerDetails = () => {
-  return <div>CricketerDetails</div>;
+  const context = useContext(AppContext);
+  const { id } = useParams();
+
+  return <div className="p-4">
+    <CricketerCard {...context?.app.players.find(pl => pl.id === id)} />
+  </div>;
 };
 
 export default CricketerDetails;
