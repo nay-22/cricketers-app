903d54cd0d5831bb1ac4a7b9cb9c70a1d6701338
Nayan <nayan92001@gmail.com>
2025-02-13 10:28:34 +0530

    feat(Header): implement header that allows theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import Switch from "./Switch";
+import AppContext from "../context/AppContext";
+
 const Header = () => {
-  return <div className="">Header</div>;
+  const context = useContext(AppContext);
+
+  const toggleTheme = () => {
+    context?.setApp(prev => ({
+      ...prev,
+      theme: prev.theme === 'dark' ? 'light' : 'dark'
+    }))
+  }
+
+  return <div className="bg-slate-300 p-4 flex items-center justify-between">
+    <div>CricketZ</div>
+    <div><Switch on={context?.app.theme === 'dark'} onClick={toggleTheme} /></div>
+  </div>;
 };
 
 export default Header;
