4c0404441e1c4fc524ccdb2cad3af697e555dc2e
Nayan <nayan92001@gmail.com>
2025-02-14 01:27:05 +0530

    feat(SearchResultView): implement custom result view to use in SearchForm

---
+++ src/components/SearchResultView.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const SearchResultView = ({ name, id }: { name: string; id: string }) => {
+  return (
+    <Link to={`/cricketer/${id}`}>
+      <div className="p-2">{name}</div>
+    </Link>
+  );
+};
+
+export default SearchResultView;
