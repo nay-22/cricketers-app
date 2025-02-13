2e8c135e8447fe89be7182565deff4c7e484a3d9
Nayan <nayan92001@gmail.com>
2025-02-13 18:23:52 +0530

    feat(Cricketers): add FilterForm that open in a Modal; FilterForm is lazy loaded

--- src/pages/Cricketers.tsx
+++ src/pages/Cricketers.tsx
@@ -1,54 +1,85 @@
-import { FC, useContext, useEffect, useState } from "react"
-import AppContext from "../context/AppContext"
+import { FC, lazy, Suspense, useContext, useEffect, useState } from "react";
+import AppContext from "../context/AppContext";
 import CricketerCard from "../components/cards/CricketerCard";
 import { TPlayer } from "../types";
 import Paginator from "../components/Paginator";
 import { Link } from "react-router-dom";
+import FilterIcon from "../assets/icons/FilterIcon";
+import Modal from "../components/Modal";
+
+const FilterForm = lazy(() => import("../forms/FilterForm"));
 
 export type CricketersProps = {
   itemsPerPage?: number;
-}
+};
 
 /**
  * TODO:
- * - Filter
- * - Sorting
- * - Pagination
- * @returns 
+ * - Search(TypeAhead)
+ * @returns
  */
 const Cricketers: FC<CricketersProps> = ({ itemsPerPage = 10 }) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   const context = useContext(AppContext);
 
   if (!context) {
     throw new Error("Context undefined");
   }
 
-  const [paginatedData, setPaginatedData] = useState<TPlayer[]>(context.app.players.slice(0, itemsPerPage))
+  const { app } = context;
+
+  const [paginatedData, setPaginatedData] = useState<TPlayer[]>(
+    app.players.slice(0, itemsPerPage)
+  );
+
+  useEffect(() => {
+    setPaginatedData(app.players.slice(0, itemsPerPage));
+  }, [app.players, itemsPerPage]);
 
   const handlePageChange = (page: number, limit: number) => {
     const start = page * limit;
     const end = start + limit;
-    setPaginatedData(context.app.players.slice(start, end));
-  }
+    setPaginatedData(app.players.slice(start, end));
+  };
 
-  useEffect(() => {
-    setPaginatedData(context.app.players.slice(0, itemsPerPage));
-  }, [context.app.players, itemsPerPage])
+  return (
+    <>
+      <header className="p-2 sticky top-0 bg-white/60 backdrop-blur-[5px] flex items-center justify-between">
+        <div></div>
+        <Paginator
+          className="grow"
+          showControls
+          showJumpControls
+          limit={itemsPerPage}
+          items={app.players.length}
+          onClick={handlePageChange}
+        />
+        <button
+          className="hover:cursor-pointer hover:bg-gray-100 rounded-lg p-1"
+          onClick={() => setShowModal(true)}
+        >
+          <FilterIcon className="stroke-amber-600" />
+        </button>
+      </header>
+      <div className="flex items-start gap-4 flex-col p-2">
+        {paginatedData.map(({ description, ...rest }: TPlayer, i) => (
+          <Link
+            key={`${i}-${rest.id}`}
+            className="w-full"
+            to={`/cricketer/${rest.id}`}
+          >
+            <CricketerCard {...rest} />
+          </Link>
+        ))}
+      </div>
+      <Modal open={showModal} onClose={() => setShowModal(false)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <FilterForm />
+        </Suspense>
+      </Modal>
+    </>
+  );
+};
 
-  return <>
-    <header>
-      <Paginator showControls showJumpControls limit={itemsPerPage} items={context.app.players.length} onClick={handlePageChange} />
-    </header>
-    <div className="flex items-start gap-4 flex-col p-2">
-      {paginatedData.map(({ description, ...rest }: TPlayer, i) => <Link
-        className="w-full"
-        to={`/cricketer/${rest.id}`}
-      >
-        <CricketerCard key={`${i}-${rest.id}`} {...rest} />
-      </Link>
-      )}
-    </div>
-  </>;
-}
-
-export default Cricketers
+export default Cricketers;
