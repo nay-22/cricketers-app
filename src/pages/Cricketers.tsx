b91faf85fa6186569bbf77d7f3403b662ee481c0
Nayan <nayan92001@gmail.com>
2025-02-13 11:12:30 +0530

    feat(Cricketers): add logic to render list of CricketerCards from the context

--- src/pages/Cricketers.tsx
+++ src/pages/Cricketers.tsx
@@ -1,7 +1,23 @@
+import { useContext } from "react"
+import AppContext from "../context/AppContext"
+import CricketerCard from "../components/cards/CricketerCard";
+import { TPlayer } from "../types";
+
+/**
+ * TODO:
+ * - Filter
+ * - Sorting
+ * - Pagination
+ * @returns 
+ */
 const Cricketers = () => {
-  return (
-    <div>Cricketers</div>
-  )
+  const context = useContext(AppContext);
+
+  return <>
+    <div className="flex items-start gap-4 flex-col p-2">
+      {context?.app.players.map(({ description, ...rest }: TPlayer) => <CricketerCard {...rest} />)}
+    </div>
+  </>;
 }
 
 export default Cricketers
