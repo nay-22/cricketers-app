f96302acf66d4c1a86bf14502353df5244040241
Nayan <nayan92001@gmail.com>
2025-02-14 01:26:27 +0530

    feat(SearchForm): implement scalable typeahead SearchForm

---
+++ src/components/forms/SearchForm.tsx
@@ -0,0 +1,105 @@
+import { ComponentType, useEffect, useState } from "react";
+import SearchIcon from "../../assets/icons/SearchIcon";
+
+export type SearchFormProps<T, U, V> = {
+  dataFetcher: (q: string) => Promise<T>;
+  dataExtractor: (data: T) => U[];
+  resultExtractor: (res: U) => V;
+  resultView: ComponentType<V>;
+  onClick?: (res: U) => void;
+};
+
+/**
+ * TODO:
+ * - Apply local and session caching
+ * - Add keyboard navigation
+ */
+const SearchForm = <T, U, V>({
+  dataFetcher,
+  dataExtractor,
+  resultExtractor,
+  resultView,
+  onClick,
+}: SearchFormProps<T, U, V>) => {
+  const [data, setData] = useState<T | undefined>(undefined);
+  const [input, setInput] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showRes, setShowRes] = useState<boolean>(false);
+  const [results, setResults] = useState<U[] | undefined>(undefined);
+
+  const executeFetch = async () => {
+    try {
+      const response = await dataFetcher(input);
+      setData(response);
+      setResults(dataExtractor(response));
+    } catch (e: any) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleResultClick = (res: U) => {
+    if (onClick) onClick(res);
+    setInput("");
+    setResults(undefined);
+    setData(undefined);
+  };
+
+  useEffect(() => {
+    let timer: number;
+    if (input === "") {
+      setIsLoading(false);
+      setResults(undefined);
+      setData(undefined);
+    } else {
+      setIsLoading(true);
+    }
+    if (input) {
+      timer = setTimeout(() => executeFetch(), 350);
+    }
+
+    return () => clearTimeout(timer);
+  }, [input]);
+
+  const ResultViewComponent = resultView;
+
+  return (
+    <div className="relative w-full">
+      <div
+        className={`flex items-center justify-center border-1 border-gray-200 px-2 py-2 w-full ${
+          !showRes ? "rounded-lg" : "rounded-t-lg"
+        }`}
+      >
+        <SearchIcon width={25} />
+        <input
+          onFocus={() => setShowRes(true)}
+          onBlur={() => setTimeout(() => setShowRes(false), 200)}
+          type="search"
+          placeholder="Type player name..."
+          className="w-full outline-0"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      {data && showRes && (
+        <div className="absolute w-full white/40 backdrop-blur-2xl rounded-b-lg z-35">
+          {isLoading ? (
+            <div className="p-2 flex items-center justify-center">
+              Loading...
+            </div>
+          ) : (
+            results &&
+            results.map((res, i) => (
+              <div key={`wrapper-${i}`} onClick={() => handleResultClick(res)}>
+                <ResultViewComponent key={i} {...resultExtractor(res)} />
+              </div>
+            ))
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchForm;
