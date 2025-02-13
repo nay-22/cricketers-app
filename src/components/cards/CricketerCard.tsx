7dddb9a3cf213dd270634403ede15f77e7a29bfa
Nayan <nayan92001@gmail.com>
2025-02-13 11:11:24 +0530

    feat(CricketerCard): implement CricketerCard that renders the props provided

---
+++ src/components/cards/CricketerCard.tsx
@@ -0,0 +1,26 @@
+// name, description, type, points, rank(derived), dob, age(derived)
+
+import { FC } from "react"
+import { TPlayer } from "../../types"
+import Chip from "../Chip"
+
+const CricketerCard: FC<TPlayer> = ({ id, name, description, type, points, rank, dob }) => {
+    return (
+        <div className="rounded-xl shadow-2xl w-full">
+            <div className="bg-gray-300 rounded-t-xl p-2 flex items-center justify-between">
+                {name && <div className="text-xl font-semibold">{name}</div>}
+                {dob && <div className="font-semibold text-gray-600">{new Date(dob).toLocaleDateString()}</div>}
+            </div>
+            <div className="bg-slate-200">
+                {description && <div className="p-2">{description}</div>}
+                <div className="p-2 bg-gray-300 rounded-b-2xl flex items-center justify-start gap-2">
+                    {type && <Chip className="bg-slate-500 text-white">Type: {type}</Chip>}
+                    {points && <Chip className="bg-slate-500 text-white">Points: {points}</Chip>}
+                    {rank && <Chip className="bg-slate-500 text-white">Rank: {rank}</Chip>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CricketerCard
