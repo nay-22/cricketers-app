571e41c1cff518f42e23b95b8b8e904c7ca003fc
Nayan <nayan92001@gmail.com>
2025-02-14 16:41:50 +0530

    fix(CricketerCard): render age instead of dob

--- src/components/cards/CricketerCard.tsx
+++ src/components/cards/CricketerCard.tsx
@@ -1,8 +1,7 @@
-// name, description, type, points, rank(derived), dob, age(derived)
-
 import { FC } from "react";
 import { TPlayer } from "../../types";
 import Chip from "../Chip";
+import getAge from "../../utils/get-age";
 
 const CricketerCard: FC<TPlayer> = ({
   id,
@@ -16,20 +15,34 @@ const CricketerCard: FC<TPlayer> = ({
   return (
     <div className="rounded-xl w-full min-w-[250px]">
       <div className="bg-gray-300 rounded-t-xl p-2 flex items-center justify-between">
-        {name && <div className="text-lg sm:text-2xl font-semibold overflow-hidden whitespace-nowrap text-ellipsis">{name}</div>}
+        {name && (
+          <div className="text-lg sm:text-2xl font-semibold overflow-hidden whitespace-nowrap text-ellipsis">
+            {name}
+          </div>
+        )}
         {dob && (
           <div className="font-semibold text-gray-600">
-            {new Date(dob).toLocaleDateString()}
+            Age: {getAge(new Date(dob).getTime())}
           </div>
         )}
       </div>
       {description && <div className="p-2">{description}</div>}
       <div className="p-2 bg-gray-300 rounded-b-2xl flex items-center justify-start gap-2">
-        {type && <Chip className="bg-amber-400 font-semibold text-xs sm:text-lg">Type: {type}</Chip>}
-        {points && (
-          <Chip className="bg-slate-500 text-white text-xs sm:text-lg">Points: {points}</Chip>
+        {type && (
+          <Chip className="bg-amber-400 font-semibold text-xs sm:text-lg">
+            Type: {type}
+          </Chip>
         )}
-        {rank && <Chip className="bg-slate-500 text-white text-xs sm:text-lg">Rank: {rank}</Chip>}
+        {points && (
+          <Chip className="bg-slate-500 text-white text-xs sm:text-lg">
+            Points: {points}
+          </Chip>
+        )}
+        {rank && (
+          <Chip className="bg-slate-500 text-white text-xs sm:text-lg">
+            Rank: {rank}
+          </Chip>
+        )}
       </div>
     </div>
   );
