aa9d8ce9342fb1231d9b2c1df7a19b4f2b737ea4
Nayan <nayan92001@gmail.com>
2025-02-14 16:42:27 +0530

    feat(main): lazy load route components

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,41 @@
-import { StrictMode } from "react";
+import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cricketers from "./pages/Cricketers.tsx";
-import Error from "./pages/Error.tsx";
-import CricketerDetails from "./pages/CricketerDetails.tsx";
 import App from "./App.tsx";
+
+const Error = lazy(() => import("./pages/Error.tsx"));
+
+const CricketerDetails = lazy(() => import("./pages/CricketerDetails.tsx"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <Error />,
+    errorElement: (
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center">Loading...</div>
+        }
+      >
+        <Error />
+      </Suspense>
+    ),
     children: [
       { index: true, element: <Cricketers /> },
-      { path: "cricketer/:id", element: <CricketerDetails /> },
+      {
+        path: "cricketer/:id",
+        element: (
+          <Suspense
+            fallback={
+              <div className="flex items-center justify-center">Loading...</div>
+            }
+          >
+            <CricketerDetails />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
