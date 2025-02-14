1315ce0e4409fd313b74b7f01a0d58fcf1ac1e69
Nayan <nayan92001@gmail.com>
2025-02-14 18:09:36 +0530

    feat(theme): add theme styles

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -1,59 +1,59 @@
 import { ThemeOptions } from "../types";
 
-export const lightTheme: ThemeOptions = {
+export const darkTheme: ThemeOptions = {
   background: {
-    primary: "",
-    secondary: "",
-    tertiary: "",
-    accent: "",
+    primary: "bg-dark-bg-primary",
+    secondary: "bg-dark-bg-secondary",
+    tertiary: "bg-dark-bg-tertiary",
+    accent: "bg-amber-500",
   },
   text: {
-    primary: "",
-    secondary: "",
-    tertiary: "",
-    accent: "",
+    primary: "text-dark-text-primary",
+    secondary: "text-dark-text-secondary",
+    tertiary: "text-dark-text-tertiary",
+    accent: "text-dark-text-accent",
+  },
+  border: {
+    primary: "border-dark-text-primary",
+    secondary: "border-dark-text-secondary",
+    tertiary: "border-dark-text-tertiary",
+    accent: "border-dark-text-accent",
   },
   error: {
     background: {
-      primary: "",
-      secondary: "",
-      tertiary: "",
-      accent: "",
+      primary: "bg-red-950",
     },
     text: {
-      primary: "",
-      secondary: "",
-      tertiary: "",
-      accent: "",
+      primary: "text-gray-400",
     },
   },
 };
 
-export const darkTheme: ThemeOptions = {
+export const lightTheme: ThemeOptions = {
   background: {
-    primary: "",
-    secondary: "",
-    tertiary: "",
-    accent: "",
+    primary: "bg-light-bg-primary",
+    secondary: "bg-light-bg-secondary",
+    tertiary: "bg-light-bg-tertiary",
+    accent: "bg-amber-400",
   },
   text: {
-    primary: "",
-    secondary: "",
-    tertiary: "",
-    accent: "",
+    primary: "text-light-text-primary",
+    secondary: "text-light-text-secondary",
+    tertiary: "text-light-text-tertiary",
+    accent: "text-light-text-accent",
+  },
+  border: {
+    primary: "border-light-text-primary",
+    secondary: "border-light-text-secondary",
+    tertiary: "border-light-text-tertiary",
+    accent: "border-light-text-accent",
   },
   error: {
     background: {
-      primary: "",
-      secondary: "",
-      tertiary: "",
-      accent: "",
+      primary: "bg-red-300",
     },
     text: {
-      primary: "",
-      secondary: "",
-      tertiary: "",
-      accent: "",
+      primary: "text-gray-800",
     },
   },
 };
