7a41a579db728d02c5c314991886938cf7572e5f
Nayan <nayan92001@gmail.com>
2025-02-16 17:16:44 +0530

    feat(SearchForm): add keyboard navigation support for search results

--- src/components/forms/SearchForm.tsx
+++ src/components/forms/SearchForm.tsx
@@ -1,11 +1,35 @@
-import { useEffect, useState } from "react";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
 import SearchIcon from "../../assets/icons/SearchIcon";
 import { SearchFormProps } from "../../types";
 
 /**
- * TODO:
- * - Apply local and session caching
- * - Add keyboard navigation
+ * SearchForm Component
+ *
+ * A generic search form that fetches and displays search results based on user input with support for
+ * keyboard navigation.
+ * 
+ * @component
+ *
+ * @template T - Type of the data fetched from dataFetcher
+ * @template U - Type of the individual result item
+ * @template V - Type of the props passed to the resultView component
+ *
+ * @param {Object} props - Component props
+ * @param {(q: string) => Promise<T>} props.dataFetcher - Asynchronous function to fetch data based on input
+ * @param {(data: T) => U[]} props.dataExtractor - Function to extract an array of results from fetched data
+ * @param {(res: U) => V} props.resultExtractor - Function to transform a result item to props for the result view
+ * @param {ComponentType<V>} props.resultView - Component to render each search result
+ * @param {(res: U) => void} [props.onClick] - Callback function triggered when a search result is clicked
+ * @param {Object} [props.styleOptions] - Custom style options for input, icon, and wrapper
+ * @param {Object} [props.styleOptions.wrapper] - Style options for the wrapper
+ * @param {string} [props.styleOptions.wrapper.className] - CSS class name for the wrapper
+ * @param {CSSProperties} [props.styleOptions.wrapper.style] - Inline styles for the wrapper
+ * @param {Object} [props.styleOptions.icon] - Style options for the icon
+ * @param {string} [props.styleOptions.icon.className] - CSS class name for the icon
+ * @param {CSSProperties} [props.styleOptions.icon.style] - Inline styles for the icon
+ * @param {boolean} [props.isDark=false] - Flag to apply dark mode styling
+ * 
+ * @returns The search form component
  */
 const SearchForm = <T, U, V>({
   dataFetcher,
@@ -22,6 +46,10 @@ const SearchForm = <T, U, V>({
   const [showRes, setShowRes] = useState<boolean>(false);
   const [results, setResults] = useState<U[] | undefined>(undefined);
 
+  const [currIdx, setCurrIdx] = useState<number>(-1);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const executeFetch = async () => {
     try {
       const response = await dataFetcher(input);
@@ -34,11 +62,28 @@ const SearchForm = <T, U, V>({
     }
   };
 
-  const handleResultClick = (res: U) => {
-    if (onClick) onClick(res);
+  const reset = () => {
     setInput("");
     setResults(undefined);
     setData(undefined);
+  };
+
+  const handleResultClick = (res: U) => {
+    if (onClick) onClick(res);
+    reset();
+  };
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowUp" && results) {
+      setCurrIdx((prev) => (prev - 1 >= 0 ? prev - 1 : prev));
+    } else if (e.key === "ArrowDown" && results) {
+      setCurrIdx((prev) => (prev + 1 < results?.length ? prev + 1 : prev));
+    } else if (e.key === "Enter" && results && onClick) {
+      console.log(results[currIdx]);
+      onClick(results[currIdx]);
+      inputRef.current?.blur();
+      reset();
+    }
   };
 
   useEffect(() => {
@@ -72,6 +117,7 @@ const SearchForm = <T, U, V>({
           width={25}
         />
         <input
+          ref={inputRef}
           onFocus={() => setShowRes(true)}
           onBlur={() => setTimeout(() => setShowRes(false), 200)}
           type="search"
@@ -81,10 +127,11 @@ const SearchForm = <T, U, V>({
           }`}
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyPress}
         />
       </div>
       {data && showRes && (
-        <div className="absolute w-full white/40 backdrop-blur-2xl rounded-b-lg z-35">
+        <div className="absolute w-full backdrop-blur-xl rounded-b-lg z-35">
           {isLoading ? (
             <div className="p-2 flex items-center justify-center">
               Loading...
@@ -92,7 +139,13 @@ const SearchForm = <T, U, V>({
           ) : (
             results &&
             results.map((res, i) => (
-              <div key={`wrapper-${i}`} onClick={() => handleResultClick(res)}>
+              <div
+                className={`overflow-hidden ${
+                  i === currIdx ? "bg-white/30 backdrop-blur-2xl" : ""
+                } last:rounded-b-lg`}
+                key={`wrapper-${i}`}
+                onClick={() => handleResultClick(res)}
+              >
                 <ResultViewComponent key={i} {...resultExtractor(res)} />
               </div>
             ))
